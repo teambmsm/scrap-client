807a9feff020513422e6db97eacfeb6fd2bf6485
sumi-0011 <selina2000@naver.com>
2023-01-22 00:16:15 +0900

    feat: header create

---
+++ client/components/icon/hamburger.tsx
@@ -0,0 +1,29 @@
+function HamburgerIcon() {
+  return (
+    <svg
+      width="30"
+      height="23"
+      viewBox="0 0 30 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_66_1015)">
+        <path
+          d="M3.75 17.2398H26.25V15.3265H3.75V17.2398ZM3.75 12.4566H26.25V10.5434H3.75V12.4566ZM3.75 5.76019V7.67346H26.25V5.76019H3.75Z"
+          fill="#263238"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_66_1015">
+          <rect
+            width="30"
+            height="22.9592"
+            fill="white"
+            transform="translate(0 0.0203857)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+export default HamburgerIcon;
--- client/components/layout.tsx
+++ client/components/layout.tsx
@@ -1,7 +1,28 @@
+import { ReactNode } from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
+import HamburgerIcon from 'client/components/icon/hamburger';
 
-function Layout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+interface LayoutProps {
+  children: ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  const handleMenuClick = () => {
+    console.log('menu click');
+  };
+
+  return (
+    <Wrapper>
+      <HeaderWrapper>
+        <Image src={'/logo.png'} alt="scrap" width="120" height="40" />
+        <MenuWrapper onClick={handleMenuClick}>
+          <HamburgerIcon />
+        </MenuWrapper>
+      </HeaderWrapper>
+      {children}
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
@@ -10,5 +31,17 @@ const Wrapper = styled.div`
   width: 100%;
   background-color: #fff;
   height: 100vh;
+`;
+
+const HeaderWrapper = styled.header`
+  padding: 45px 15px 15px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MenuWrapper = styled.div`
+  cursor: pointer;
 `;
 export default Layout;
