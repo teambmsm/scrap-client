caf1c87beefb62e3689c4a7a9804fc593e4a690e
sumi-0011 <selina2000@naver.com>
2023-01-23 01:49:36 +0900

    fix: json server remove

--- src/client/pages/main/index.tsx
+++ src/client/pages/main/index.tsx
@@ -5,13 +5,60 @@ import Layout from '@/client/components/layout';
 import { PostType } from '@/client/types/post';
 import { requestGET } from '@/client/api';
 
+const post = [
+  {
+    id: '1',
+    url: '',
+    title: '수미칩은 맛있다. 왜냐하면 맛있기 때문이다.',
+    tags: ['수미칩', '세벳돈 조아요 >-<'],
+    image:
+      'https://images.unsplash.com/photo-1673379324645-21a670fc74c8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODQwNTZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzQzNzgwMDI&ixlib=rb-4.0.3&q=80&w=400',
+    author: '수미칩',
+    description: '수미칩은 맛있다. 왜냐하면 수미칩은 맛있기 때문이다.',
+    view: 0,
+  },
+  {
+    id: '2',
+    url: '',
+    title: '수미칩은 맛있다. 왜냐하면 맛있기 때문이다.',
+    tags: ['수미칩', '세벳돈 조아요 >-<'],
+    image:
+      'https://images.unsplash.com/photo-1673379324645-21a670fc74c8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODQwNTZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzQzNzgwMDI&ixlib=rb-4.0.3&q=80&w=400',
+    author: '수미칩',
+    description: '수미칩은 맛있다. 왜냐하면 수미칩은 맛있기 때문이다.',
+    view: 0,
+  },
+  {
+    id: '3',
+    url: '',
+    title: '수미칩은 맛있다. 왜냐하면 맛있기 때문이다.',
+    tags: ['수미칩', '세벳돈 조아요 >-<'],
+    image:
+      'https://images.unsplash.com/photo-1673379324645-21a670fc74c8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODQwNTZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzQzNzgwMDI&ixlib=rb-4.0.3&q=80&w=400',
+    author: '수미칩',
+    description: '수미칩은 맛있다. 왜냐하면 수미칩은 맛있기 때문이다.',
+    view: 0,
+  },
+  {
+    id: '4',
+    url: '',
+    title: '수미칩은 맛있다. 왜냐하면 맛있기 때문이다.',
+    tags: ['수미칩', '세벳돈 조아요 >-<'],
+    image:
+      'https://images.unsplash.com/photo-1673379324645-21a670fc74c8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzODQwNTZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzQzNzgwMDI&ixlib=rb-4.0.3&q=80&w=400',
+    author: '수미칩',
+    description: '수미칩은 맛있다. 왜냐하면 수미칩은 맛있기 때문이다.',
+    view: 0,
+  },
+];
 function Main() {
   const [posts, setPosts] = useState<PostType[]>([]);
 
   const getPosts = async () => {
-    const data = await requestGET('/post');
-    setPosts(data);
+    // const data = await requestGET('/post');
+    setPosts(post);
   };
+
   useEffect(() => {
     getPosts();
   }, []);
